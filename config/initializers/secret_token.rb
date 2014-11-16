375a655a727b9eb2e635752c4304baba670201c8
Group007 <909138412@qq.com>
2014-11-16 15:42:06 +0800

    add rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HomeworkEquipemt::Application.config.secret_token = '99c94b08ea6b215808b5842d586f24a9a00c74a4ca381cb5c2513853231cbb6c0dbea0b003bc1158b6737ca8d444c4fc333d6f961eb5931a6adde713437198c0'
